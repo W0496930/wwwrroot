d7058de7648af07af929359bb047e2974b8d37e6
William Taylor <144703862+W0496930@users.noreply.github.com>
2023-12-05 09:11:00 -0400

    Update Ride_Sharing.js

--- RideSharingSite/Ride_Sharing.js
+++ RideSharingSite/Ride_Sharing.js
@@ -1,20 +1,24 @@
-//Establishing the accordion in the FAQ Section
-var acc = document.getElementsByClassName("accordion");
-var i;
+// Accordion for FAQ Section
+var accordions = document.querySelectorAll(".accordion");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+accordions.forEach(function (accordion) {
+  accordion.addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
-}
+});
 
-//Added this line of code to prevent the home link from being highlighted by default on page load.
+// Remove 'active' class from the home link on page load
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('.nav-item.active').classList.remove('active');
+  var activeNavItem = document.querySelector('.nav-item.active');
+
+  if (activeNavItem) {
+    activeNavItem.classList.remove('active');
+  }
 });
